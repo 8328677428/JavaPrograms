279f9e441d533cd918bd62bd20b62829c7476939
8328677428 <151123628+8328677428@users.noreply.github.com>
2023-11-17 12:26:51 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,17 @@
+// instance variable.
+package com.java.varibale;
+
+public class InstanceVariable {
+
+	int number;
+
+	public static void main(String[] args) {
+		// create object
+//		Object obj = new Object();
+		// object
+		// syntax ==> localVariableValue
+		InstanceVariable iv = new InstanceVariable();
+		System.out.println(iv.number);
+	}
+
+}
